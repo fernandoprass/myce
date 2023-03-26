f42a9085e99fcc18dad8fbe408c444fa966db2e0
fernandoprass <fprass@gmail.com>
2023-03-26 13:56:29 +0100

    Add IO.File wrappers

--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 MYCE (Makes Your Coding Easier) is a collection of NuGet packages with a set of useful functions
 
 ## [MYCE.Extensions](https://github.com/fernandoprass/myce/tree/main/sources/Myce.Extensions)
-Montains a set of extensions for the most common types.
+A set of extensions for the most common types.
 
 ## [MYCE.Response](https://github.com/fernandoprass/myce/tree/main/sources/Myce.Response)
 A set of classes to be used as a return in APIs.
 
 ## [MYCE.Validation](https://github.com/fernandoprass/myce/tree/main/sources/Myce.Validation)
 A fluent validation library.
+
+## [MYCE.Wrappers](https://github.com/fernandoprass/myce/tree/main/sources/Myce.Wrappers)
+A set of wrappers for the most common IO classes, it is usefull to mock methods in unit tests
 
 ## Contributions
 Contributions are welcome on this project. If you have an extension that you think is useful and can make life easier for other developers, even if it's for a type not present in the package yet, create a Pull Request and submit it.
---
+++ sources/Myce.Wrappers/FileWrapper.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace Myce.Wrappers
+{
+   public class FileWrapper : IFileWrapper
+   {
+      /// <inheritdoc/>
+      public bool Exists(string path)
+      {
+         return File.Exists(path);
+      }
+
+      /// <inheritdoc/>
+      public string ReadAllText(string path)
+      {
+         return File.ReadAllText(path);
+      }
+
+      /// <inheritdoc/>
+      public string ReadAllText(string path, Encoding encoding)
+      {
+         return File.ReadAllText(path, encoding);
+      }
+
+      /// <inheritdoc/>
+      public Task<string> ReadAllTextAsync(string path, CancellationToken cancellationToken = default)
+      {
+         return File.ReadAllTextAsync(path, cancellationToken);
+      }
+
+      /// <inheritdoc/>
+      public Task<string> ReadAllTextAsync(string path, Encoding encoding, CancellationToken cancellationToken = default)
+      {
+         return File.ReadAllTextAsync(path, encoding, cancellationToken);
+      }
+
+      /// <inheritdoc/>
+      public void WriteAllText(string path, string? contents)
+      {
+         File.WriteAllText(path, contents);
+      }
+
+      /// <inheritdoc/>
+      public void WriteAllText(string path, string? contents, Encoding encoding)
+      {
+         File.WriteAllText(path, contents, encoding);
+      }
+
+      /// <inheritdoc/>
+      public Task WriteAllTextAsync(string path, string? contents, CancellationToken cancellationToken = default)
+      {
+         return File.WriteAllTextAsync(path, contents, cancellationToken);
+      }
+
+      /// <inheritdoc/>
+      public Task WriteAllTextAsync(string path, string? contents, Encoding encoding, CancellationToken cancellationToken = default)
+      {
+         return File.WriteAllTextAsync(path, contents, encoding, cancellationToken);
+      }
+   }
+}
