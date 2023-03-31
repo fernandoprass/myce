d56327bd9d593bae7e3959350335f2d9881cabed
fernandoprass <fprass@gmail.com>
2023-03-31 13:35:35 +0100

    Add Create and Exists methods to FileWrapper

--- sources/Myce.Wrappers/Contracts/IFileWrapper.cs
+++ sources/Myce.Wrappers/Contracts/IFileWrapper.cs
@@ -2,83 +2,107 @@
 
 namespace Myce.Wrappers.Contracts
 {
-    public interface IFileWrapper
-    {
-        /// <summary>
-        /// Determines whether the specified file exists.
-        /// </summary>
-        /// <param name="path">The file to check.</param>
-        /// <returns>TRUE if the caller has the required permissions and path contains the name of an existing file; otherwise, FALSE. 
-        /// This method also returns FALSE if path is null, an invalid path, or a zero-length string. If the caller does not have sufficient 
-        /// permissions to read the specified file, no exception is thrown and the method returns FALSE regardless of the existence of path.</returns>
-        bool Exists(string path);
+   public interface IFileWrapper
+   {
+      /// <summary>
+      /// Creates or overwrites a file in the specified path.
+      /// </summary>
+      /// <param name="path">The path and name of the file to create.</param>
+      /// <returns>A FileStream that provides read/write access to the file specified in path.</returns>
+      FileStream Create(string path);
 
-        /// <summary>
-        /// Opens a text file, reads all the text in the file, and then closes the file.
-        /// </summary>
-        /// <param name="path">The file to open for reading.</param>
-        /// <returns>A string containing all the text in the file.</returns>
-        string ReadAllText(string path);
+      /// <summary>
+      /// Creates or overwrites a file in the specified path, specifying a buffer size.
+      /// </summary>
+      /// <param name="path">The path and name of the file to create.</param>
+      /// <param name="bufferSize">The number of bytes buffered for reads and writes to the file.</param>
+      /// <returns>A FileStream with the specified buffer size that provides read/write access to the file specified in path.</returns>
+      FileStream Create(string path, int bufferSize);
 
-        /// <summary>
-        /// Opens a file, reads all text in the file with the specified encoding, and then closes the file.
-        /// </summary>
-        /// <param name="path">The file to open for reading.</param>
-        /// <param name="encoding">The encoding applied to the contents of the file.</param>
-        /// <returns>A string containing all the text in the file.</returns>
-        string ReadAllText(string path, Encoding encoding);
+      /// <summary>
+      /// Creates or overwrites a file in the specified path, specifying a buffer size and options that describe how to create or overwrite the file.
+      /// </summary>
+      /// <param name="path">The path and name of the file to create.</param>
+      /// <param name="bufferSize">The number of bytes buffered for reads and writes to the file.</param>
+      /// <param name="options">One of the FileOptions values that describes how to create or overwrite the file.</param>
+      /// <returns>A new file with the specified buffer size.</returns>
+      FileStream Create(string path, int bufferSize, System.IO.FileOptions options);
 
-        /// <summary>
-        /// Asynchronously opens a text file, reads all the text in the file, and then closes the file.
-        /// </summary>
-        /// <param name="path">The file to open for reading.</param>
-        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
-        /// <returns>A string containing all the text in the file.</returns>
-        Task<string> ReadAllTextAsync(string path, CancellationToken cancellationToken = default);
+      /// <summary>
+      /// Determines whether the specified file exists.
+      /// </summary>
+      /// <param name="path">The file to check.</param>
+      /// <returns>TRUE if the caller has the required permissions and path contains the name of an existing file; otherwise, FALSE. 
+      /// This method also returns FALSE if path is null, an invalid path, or a zero-length string. If the caller does not have sufficient 
+      /// permissions to read the specified file, no exception is thrown and the method returns FALSE regardless of the existence of path.</returns>
+      bool Exists(string path);
 
-        /// <summary>
-        /// Asynchronously opens a text file, reads all text in the file with the specified encoding, and then closes the file.
-        /// </summary>
-        /// <param name="path">The file to open for reading.</param>
-        /// <param name="encoding">The encoding applied to the contents of the file.</param>
-        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
-        /// <returns>A string containing all the text in the file.</returns>
-        Task<string> ReadAllTextAsync(string path, Encoding encoding, CancellationToken cancellationToken = default);
+      /// <summary>
+      /// Opens a text file, reads all the text in the file, and then closes the file.
+      /// </summary>
+      /// <param name="path">The file to open for reading.</param>
+      /// <returns>A string containing all the text in the file.</returns>
+      string ReadAllText(string path);
 
-        /// <summary>
-        /// Creates a new file, write the contents to the file, and then closes the file. If the target file already exists, it is overwritten.
-        /// </summary>
-        /// <param name="path">The file to write to.</param>
-        /// <param name="contents">The string to write to the file.</param>
-        void WriteAllText(string path, string? contents);
+      /// <summary>
+      /// Opens a file, reads all text in the file with the specified encoding, and then closes the file.
+      /// </summary>
+      /// <param name="path">The file to open for reading.</param>
+      /// <param name="encoding">The encoding applied to the contents of the file.</param>
+      /// <returns>A string containing all the text in the file.</returns>
+      string ReadAllText(string path, Encoding encoding);
 
-        /// <summary>
-        /// Creates a new file, write the contents to the file, and then closes the file. If the target file already exists, it is overwritten.
-        /// </summary>
-        /// <param name="path">The file to write to.</param>
-        /// <param name="contents">The string to write to the file.</param>
-        /// <param name="encoding">The encoding to apply to the string.</param>
-        void WriteAllText(string path, string? contents, Encoding encoding);
+      /// <summary>
+      /// Asynchronously opens a text file, reads all the text in the file, and then closes the file.
+      /// </summary>
+      /// <param name="path">The file to open for reading.</param>
+      /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+      /// <returns>A string containing all the text in the file.</returns>
+      Task<string> ReadAllTextAsync(string path, CancellationToken cancellationToken = default);
 
-        /// <summary>
-        /// Asynchronously creates a new file, writes the specified string to the file, and then closes the file. If the target file already exists, 
-        /// it is overwritten.
-        /// </summary>
-        /// <param name="path">The file to write to.</param>
-        /// <param name="contents">The string to write to the file.</param>
-        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
-        /// <returns>A task that represents the asynchronous write operation.</returns>
-        Task WriteAllTextAsync(string path, string? contents, CancellationToken cancellationToken = default);
+      /// <summary>
+      /// Asynchronously opens a text file, reads all text in the file with the specified encoding, and then closes the file.
+      /// </summary>
+      /// <param name="path">The file to open for reading.</param>
+      /// <param name="encoding">The encoding applied to the contents of the file.</param>
+      /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+      /// <returns>A string containing all the text in the file.</returns>
+      Task<string> ReadAllTextAsync(string path, Encoding encoding, CancellationToken cancellationToken = default);
 
-        /// <summary>
-        /// Asynchronously creates a new file, writes the specified string to the file, and then closes the file. If the target file already exists, 
-        /// it is overwritten.
-        /// </summary>
-        /// <param name="path">The file to write to.</param>
-        /// <param name="contents">The string to write to the file.</param>
-        /// <param name="encoding">The encoding to apply to the string.</param>
-        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
-        /// <returns>A task that represents the asynchronous write operation.</returns>
-        Task WriteAllTextAsync(string path, string? contents, Encoding encoding, CancellationToken cancellationToken = default);
-    }
+      /// <summary>
+      /// Creates a new file, write the contents to the file, and then closes the file. If the target file already exists, it is overwritten.
+      /// </summary>
+      /// <param name="path">The file to write to.</param>
+      /// <param name="contents">The string to write to the file.</param>
+      void WriteAllText(string path, string? contents);
+
+      /// <summary>
+      /// Creates a new file, write the contents to the file, and then closes the file. If the target file already exists, it is overwritten.
+      /// </summary>
+      /// <param name="path">The file to write to.</param>
+      /// <param name="contents">The string to write to the file.</param>
+      /// <param name="encoding">The encoding to apply to the string.</param>
+      void WriteAllText(string path, string? contents, Encoding encoding);
+
+      /// <summary>
+      /// Asynchronously creates a new file, writes the specified string to the file, and then closes the file. If the target file already exists, 
+      /// it is overwritten.
+      /// </summary>
+      /// <param name="path">The file to write to.</param>
+      /// <param name="contents">The string to write to the file.</param>
+      /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+      /// <returns>A task that represents the asynchronous write operation.</returns>
+      Task WriteAllTextAsync(string path, string? contents, CancellationToken cancellationToken = default);
+
+      /// <summary>
+      /// Asynchronously creates a new file, writes the specified string to the file, and then closes the file. If the target file already exists, 
+      /// it is overwritten.
+      /// </summary>
+      /// <param name="path">The file to write to.</param>
+      /// <param name="contents">The string to write to the file.</param>
+      /// <param name="encoding">The encoding to apply to the string.</param>
+      /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+      /// <returns>A task that represents the asynchronous write operation.</returns>
+      Task WriteAllTextAsync(string path, string? contents, Encoding encoding, CancellationToken cancellationToken = default);
+   }
 }
--- sources/Myce.Wrappers/readme.md
+++ sources/Myce.Wrappers/readme.md
@@ -2,7 +2,13 @@
 MYCE (Makes Your Coding Easier) is a Nuget package for Visual Studio that contains a set of wrappers for the most common System.IO classes, which is especially useful for mock methods in unit tests.
 
 ## Library
+This is a simple wrapper for some of the most used classes of System.IO Namespace. For more information, including examples, visit 
+[Microsoft Learn - System.IO Namespace](https://learn.microsoft.com/en-us/dotnet/api/system.io). All methods descriptions below were copied from there,
+if you find any information that is incorrect or out of date, please contact us. 
+
 Directory
+    - CreateDirectory (string path) - Creates all directories and subdirectories in the specified path unless they already exist.
+    - CreateDirectory (string path, UnixFileMode unixCreateMode) - Creates all directories and subdirectories in the specified path with the specified permissions unless they already exist.
     - Delete (string path) - Deletes an empty directory from a specified path.
     - Delete (string path, bool recursive) - Deletes the specified directory and, if indicated, any subdirectories and files in the directory.
     - Exists (string? path) - Determines whether the given path refers to an existing directory on disk.
@@ -12,6 +18,9 @@ Directory
     - GetFiles(string path, string searchPattern, SearchOption searchOption) - Returns the names of files (including their paths) that match the specified search pattern in the specified directory, using a value to determine whether to search subdirectories.
 
 - File
+    - Create(String) - Creates or overwrites a file in the specified path.
+    - Create(String, Int32)	- Creates or overwrites a file in the specified path, specifying a buffer size.
+    - Create(String, Int32, FileOptions) - Creates or overwrites a file in the specified path, specifying a buffer size and options that describe how to create or overwrite the file.
     - Exists(string path) - Determines whether the specified file exists.
     - ReadAllText(string path) - Opens a text file, reads all the text in the file, and then closes the file.
     - ReadAllTextAsync(string path, CancellationToken cancellationToken = default) - Asynchronously opens a text file, reads all the text in the file, and then closes the file.
@@ -31,4 +40,4 @@ None of this library's methods have unit test for the simple fact that, as the n
 ## Contributions
 Contributions are welcome on this library. If you use a class or method from the System.IO namespace that is not already here, create a Pull Request and submit it.
 
-Develeped by Fernando Prass
+Developed by Fernando Prass
