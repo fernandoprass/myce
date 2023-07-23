614bf60e5e2d371637e0eb262541c3acd26aeaa9
fernandoprass <fprass@gmail.com>
2023-07-23 19:01:03 +0100

    Extensions - add method IsBetween to Object class

--- sources/Myce.Extensions/ObjectExtensions.cs
+++ sources/Myce.Extensions/ObjectExtensions.cs
@@ -6,21 +6,36 @@
    public static class ObjectExtensions
    {
       /// <summary>
-      /// Return TRUE if the object is null
+      /// Check if the value of the object is null
       /// </summary>
       /// <param name="obj">The object</param>
+      /// <returns>Return TRUE if the object is null</returns>
       public static bool IsNull(this object obj)
       {
          return obj == null;
       }
 
       /// <summary>
-      /// Return TRUE if the object is NOT null
+      ///  Check if the value of the object is not null
       /// </summary>
-      /// <param name="obj">The object</param>
+      /// <param name="obj">The object value</param>
+      /// <returns>Return TRUE if the object is NOT null</returns>
       public static bool IsNotNull(this object obj)
       {
          return obj != null;
+      }
+
+      /// <summary>
+      /// Compare object values is between other two values (inclusive)
+      /// </summary>
+      /// <typeparam name="T">Any object that implements IComparable</typeparam>
+      /// <param name="value">The object value</param>
+      /// <param name="from">The FROM value</param>
+      /// <param name="to">The TO value</param>
+      /// <returns>Return TRUE if the value of the object is BETWEEN other two values (inclusive)</returns>
+      public static bool IsBetween<T>(this T value, T from, T to) where T : IComparable<T>
+      {
+         return value.CompareTo(from) >= 0 && value.CompareTo(to) <= 0;
       }
    }
 }
