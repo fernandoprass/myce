bb0ab4d7827cb9019fa0bc269221b741699d18d5
fernandoprass <fprass@gmail.com>
2026-02-06 20:25:22 +0000

    Myce.Response - Simplify Result class

--- sources/Myce.Response/Result.cs
+++ sources/Myce.Response/Result.cs
@@ -1,9 +1,16 @@
 using Myce.Extensions;
 using Myce.Response.Messages;
+using System.Text.Json.Serialization;
 
 namespace Myce.Response
 {
-   public class Result
+   public interface IResult
+   {
+      IReadOnlyCollection<Message> Messages { get; }
+      bool IsValid { get; }
+   }
+
+   public class Result : IResult
    {
       private readonly List<Message> _messages = new List<Message>();
 
@@ -11,10 +18,13 @@ namespace Myce.Response
 
       public bool IsValid => !HasError;
 
+      [JsonIgnore]
       public bool HasError => _messages.Any(x => x.Type == MessageType.Error);
 
+      [JsonIgnore]
       public bool HasMessage => _messages.Any();
 
+      [JsonIgnore]
       public bool HasWarning => _messages.Any(x => x.Type == MessageType.Warning);
 
       public string Message { get; set; }
@@ -27,6 +37,14 @@ namespace Myce.Response
 
       public Result(IEnumerable<Message> messages) => AddMessages(messages);
 
+      public static Result Success() => new Result();
+
+      public static Result Success(string message) => new Result(message);
+
+      public static Result Failure(Message message) => new Result(message);
+
+      public static Result Failure(IEnumerable<Message> messages) => new Result(messages);
+
       public void AddMessage(Message message) => _messages.Add(message);
 
       public void AddMessages(IEnumerable<Message> messages) => _messages.AddRange(messages);
@@ -34,12 +52,16 @@ namespace Myce.Response
 
    public class Result<T> : Result
    {
+      [JsonIgnore]
       public bool IsValidAndDataIsNotNull => IsValid && Data.IsNotNull();
 
+      [JsonIgnore]
       public bool IsValidAndDataIsNull => IsValid && Data.IsNull();
 
+      [JsonIgnore]
       public bool HasData => Data != null;
 
+      [JsonIgnore] 
       public bool HasErrorOrDataIsNull => !IsValidAndDataIsNotNull;
 
       public T Data { get; set; }
@@ -57,6 +79,16 @@ namespace Myce.Response
       public Result(T data, Message message) : base(message) => Data = data;
 
       public Result(T data, IEnumerable<Message> messages) : base(messages) => Data = data;
+
+      public static Result<T> Success(T data) => new Result<T>(data);
+
+      public static Result<T> Success(T data, string message) => new Result<T>(data, message);
+
+      public static Result<T> Failure(Message message) => new Result<T>(message);
+
+      public static Result<T> Failure(IEnumerable<Message> messages) => new Result<T>(messages);
+
+      public static Result<T> FromResult(Result result) => new Result<T>(result.Messages);
 
       public Result<V> ToResult<V>(Func<T, V> map)
       {
